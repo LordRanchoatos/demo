2318edb391c9d352d0f67b4cfc59e0d11c9cfb04
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-11 16:03:15 +0100

    conditional and booleans

--- index.php
+++ index.php
@@ -4,13 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margins: 0;
+            padding: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
     <?php
-    $greeting = 'Hi';
-    echo "$greeting guys";
+    $name = "Dark matter";
+    $read = false;
+    if ($read){
+        $message = "You have read $name";
+    } else {
+        $message = "You not have read $name";
+    }
+    
+    
     ?>
+    <h1>
+        <?php 
+
+            echo $message;
+        ?>
     </h1>
 </body>
 </html>
