e72f4765651919fc12e23ce482e8767931318280
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-27 11:07:23 +0100

    basic class

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<php
+<?php
 
     class Customer {
-        
+        public $id;
+        public $name;
+        public $email;
+        public $balance;
+
+        public function getCustomer($name){
+            $this->name = $name;
+            return $this->name;
+        }
     }
+
+    $customer = new Customer;
+
+    echo $customer->getCustomer('dave');
 ?>
