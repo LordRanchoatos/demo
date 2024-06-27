28df62e848288aee0d74960f4ef8c77c88939aa9
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-27 11:24:40 +0100

    private properties, public methods

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <?php
 
     class Customer {
-        public $id;
-        public $name;
-        public $email;
-        public $balance;
+        private $id;
+        private $name;
+        private $email;
+        private $balance;
         
-        public function __construct(){
-            echo 'this constructor Ran...';
-        }
-        
-        public function getCustomer($name){
+        public function __construct($id, $name, $email, $balance){
+            $this->id = $id;
             $this->name = $name;
-            return 'John doe';
+            $this->email = $email;
+            $this->balance = $balance;
         }
 
-        public function __destruct(){
-            echo 'the distructor ran...';
-        } 
-
+        public function getEmail(){
+            return $this->email;
+        }
 
     }
 
 
-    $customer = new Customer;
+    $customer = new Customer(1, "Dave", "dave@gmail.com", 0);
 
-    echo $customer->getCustomer('dave');
+
+    echo $customer->getEmail();
 ?>
