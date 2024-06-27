254e2c05b39c3704cedafb56f2c62a68da1ad766
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-27 11:16:45 +0100

    magic functions

--- index.php
+++ index.php
@@ -5,12 +5,23 @@
         public $name;
         public $email;
         public $balance;
-
+        
+        public function __construct(){
+            echo 'this constructor Ran...';
+        }
+        
         public function getCustomer($name){
             $this->name = $name;
-            return $this->name;
+            return 'John doe';
         }
+
+        public function __destruct(){
+            echo 'the distructor ran...';
+        } 
+
+
     }
+
 
     $customer = new Customer;
 
