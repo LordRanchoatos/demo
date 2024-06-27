4579ad1e57c37ca58bb28c58cdceaeb4ece5abe6
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-27 12:11:46 +0100

    static properties

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
 
-    abstract class Customer {
-        private $id;
-        private $name;
-        protected $email;
-        private $balance;
+    // abstract class Customer {
+    //     private $id;
+    //     private $name;
+    //     protected $email;
+    //     private $balance;
         
-        public function __construct($id, $name, $email, $balance){
-            $this->id = $id;
-            $this->name = $name;
-            $this->email = $email;
-            $this->balance = $balance;
-        }
+    //     public function __construct($id, $name, $email, $balance){
+    //         $this->id = $id;
+    //         $this->name = $name;
+    //         $this->email = $email;
+    //         $this->balance = $balance;
+    //     }
 
-        abstract public function getEmail();
+    //     abstract public function getEmail();
 
-    }
+    // }
 
 
     // abstract method must be in an abstract class.
@@ -26,21 +26,34 @@
 
     //echo $customer->getEmail();
 
-    class Subscriber extends Customer{
-        public $plan;
+    // class Subscriber extends Customer{
+    //     public $plan;
 
-        public function __construct($id, $name, $email, $balance, $plan){
-            parent::__construct($id, $name, $email, $balance);
-            $this->plan = $plan;
-        }
+    //     public function __construct($id, $name, $email, $balance, $plan){
+    //         parent::__construct($id, $name, $email, $balance);
+    //         $this->plan = $plan;
+    //     }
 
-        public function getEmail(){
-            return $this->email;
+    //     public function getEmail(){
+    //         return $this->email;
+    //     }
+    // }
+
+    // $subscriber = new Subscriber(1, "Dave", "dave@gmail.com", 0, 'Pro');
+
+    // echo $subscriber->getEmail();
+
+
+    class User{
+        public $username;
+        public $password;
+        public static $passwordLength = 5;
+
+        public static function getPasswordLength(){
+            return self::$passwordLength;
         }
     }
 
-    $subscriber = new Subscriber(1, "Dave", "dave@gmail.com", 0, 'Pro');
-
-    echo $subscriber->getEmail();
+    echo User::getPasswordLength();
 
 ?>
